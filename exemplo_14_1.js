1bda6c3667e53a549f32a50d2607ab62607c5420
Rael Lima <raellima01@outlook.com>
2017-04-16 16:07:39 -0300

    Create exemplo_14_1.js

---
+++ exemplo_14_1.js
@@ -0,0 +1,9 @@
+Função  evento_entrar () {
+ Alerta ( « Seja Bem Vindo » );
+}
+Função  evento_sair () {
+ Alerta ( ' Até a Próxima Visita ' );
+}
+Função  evento_clique () {
+ Documento . GetElementById ( ' id2 ' ). Estilo . Color = ' vermelho ' ;
+}
